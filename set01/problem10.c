450eb3da69d9e4a82bea7dc1d0bf165dec8c2f0b
saigawand <115694200+saigawand@users.noreply.github.com>
2022-12-08 14:04:59 +0530

    Update problem10.c

--- set01/problem10.c
+++ set01/problem10.c
@@ -1,16 +1,1 @@
-#include <stdio.h>   
-int main()  
-{  
-   char str1[10];
-   char str2[10];
-   int value;
-   printf("Enter the first string : ");  
-   scanf("%s",str1);  
-   printf("Enter the second string : ");  
-   scanf("%s",str2);
-  int i = 0
-  while (str[i] !=0, && str[i] !=0)
-    {
-      if(str[i]) != str[i])
-    }
-} 
+
